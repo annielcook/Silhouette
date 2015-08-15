838fce16854042223185498b6b6afa7e317a9333
Anna Goldberg <annalexgoldberg@gmail.com>
2015-08-15 16:51:40 -0400

    add file upload functionality

--- app/home/home.ctrl.js
+++ app/home/home.ctrl.js
@@ -1,3 +1,17 @@
-app.controller('HomeCtrl', function ($scope, $state) {
+app.controller('HomeCtrl', function ($scope, $state, HomeFactory) {
+  $scope.hello = "hello Anna";
+  $scope.uploadFile = function(event){
+    var files = event.target.files;
+    console.log('files', files);
+  };
 
+})
+
+app.factory('HomeFactory', function($http){
+  return{
+    uploadFileX: function(file){
+      console.log('here');
+      console.log('file', file);
+    }
+  }
 })
