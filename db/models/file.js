ec0b5d7a5d85a9fa135dbc17ebc11ee501f371f8
Anna Goldberg <annalexgoldberg@gmail.com>
2015-08-18 12:23:08 -0400

    pulling master

--- db/models/file.js
+++ db/models/file.js
@@ -1,11 +1,11 @@
 'use strict'
 var mongoose = require('mongoose');
-var envkeys = require('./EnvKeys');
 
 var FileSchema = new mongoose.Schema({
     name: String,
     content: String,
-    path: String
+    path: String,
+    date: String
 });
 
 
