fc87f955fb068def9d1f42e95744506fcae2b079
Anna Goldberg <annalexgoldberg@gmail.com>
2015-08-21 13:50:54 -0400

    merge filemanagerctrl and fileselectorctrl

--- app/loggedIn/files/fileManager/fileManager.ctrl.js
+++ app/loggedIn/files/fileManager/fileManager.ctrl.js
@@ -1,21 +1,25 @@
-// var mongoose = require('mongoose');
-// var File = mongoose.model('File');
-// var User = mongoose.model('User');
+var mongoose = require('mongoose');
+var File = mongoose.model('File');
+var User = mongoose.model('User');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require("fs"));
 
-window.thisApp.controller('FileManagerCtrl', function ($scope, FileManagerFactory, $rootScope) {
-   // retrieves current user's files
+window.thisApp.controller('FileManagerCtrl', function ($scope, $state, AccountEditFactory, FileManagerFactory, $rootScope) {
+
+  $scope.saveAccountChanges = AccountEditFactory.saveUserChanges;
+
+  // retrieves current user's files
   $scope.retrieveAllFiles = function(){
     FileManagerFactory.getAllFiles()
     .then(function(files){
-      $scope.files = files;
-      $scope.$digest();
+      $scope.files = files
+      console.log('$scope.files', $scope.files);
+      $scope.$digest()
     });
   }
 
   //display all files upon loading
-  $scope.retrieveAllFiles();
+  $scope.retrieveAllFiles()
 
   //upload a file and update the files displayed
   $scope.uploadFile = function(event){
@@ -23,6 +27,12 @@ window.thisApp.controller('FileManagerCtrl', function ($scope, FileManagerFactor
     .then(function(user){
       $scope.retrieveAllFiles();
     })
+  }
+  
+ $scope.fileOptions = ['.bashrc', '.bash_profile', '.gitconfig', '.zshrc'];
+ $scope.filePrefs = [];
+  $scope.addFilePreference = function(filename){
+    $scope.filePrefs = FileManagerFactory.addFilePrefs(filename, $scope.filePrefs);
   }
 
   $scope.removeFile = function(file){
@@ -65,4 +75,43 @@ window.thisApp.controller('FileManagerCtrl', function ($scope, FileManagerFactor
     })
   }
   
+  //get file preferences from user upon signup
+  $scope.addFilePrefToUser = function(){
+    $rootScope.currentUser.filePreferences = $scope.filePrefs;
+    AccountEditFactory.saveUserChanges();
+    //child process that reads files from user 
+    $scope.getFileFromUser();
+    //go to file manager state
+    $state.go('loggedIn.fileManager');
+  } 
+
+  $scope.getFileFromUser = function(){
+    var filePrefs = $rootScope.currentUser.filePreferences;
+    $scope.fileData = [];
+    var filePaths = [];
+   
+    filePrefs.forEach(function(pref, ind){
+      filePaths[ind] = process.env["HOME"]+ '/' + pref;
+    })
+
+    Promise.map(filePaths, function(path, index){
+      return Promise.all([fs.readFileAsync(path, "utf8"), path, filePrefs[index]])
+    })
+    .then(function(content){
+      $scope.fileData = content;
+      $scope.fileData.forEach(function(fileArray){
+        console.log('fileArray', fileArray);
+        FileManagerFactory.addFile(null, fileArray)
+        .then(function(createdUser){
+          console.log('createdUser', createdUser);
+          $scope.retrieveAllFiles();
+        })
+      })
+      console.log('$scope.fileData', $scope.fileData);
+    })
+    
+  }
+
 })
+
+
--- app/loggedIn/loggedIn.state.js
+++ app/loggedIn/loggedIn.state.js
@@ -1,21 +1,18 @@
 window.thisApp.config(function ($stateProvider) {
 	$stateProvider.state('loggedIn', {
 		templateUrl: __dirname + '/loggedIn.html',
-		controller: 'LoggedInCtrl',
+		controller: 'FileManagerCtrl',
 	})
 	.state('loggedIn.account', {
 		templateUrl: __dirname + '/account/account.html'
 	})
 	.state('loggedIn.fileManager', {
 		templateUrl: __dirname + '/files/fileManager/fileManager.html',
-		controller: 'FileManagerCtrl'
 	})
 	.state('loggedIn.accountEdit', {
 		templateUrl: __dirname + '/account/accountEdit.html'
 	})
 	.state('loggedIn.fileSelector', {
 		templateUrl: __dirname + '/files/fileSelector/fileSelector.html',
-		controller: 'fileCtrl',
-		controller: 'FileSelectorCtrl'
 	})
 })
