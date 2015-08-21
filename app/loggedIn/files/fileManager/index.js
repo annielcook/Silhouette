ccf886f1488f50a2c83c99665f5364f1cb58f372
Anna Goldberg <annalexgoldberg@gmail.com>
2015-08-21 10:53:46 -0400

    refactor ctrl add app/packages folders

---
+++ app/loggedIn/files/fileManager/index.js
@@ -0,0 +1,2 @@
+require(__dirname + '/fileManagerFactory.js')
+require(__dirname + '/customOnChangeDirective.js')
---
+++ app/loggedIn/files/index.js
@@ -0,0 +1,2 @@
+require(__dirname + '/fileManager');
+require(__dirname + '/file.ctrl.js');
