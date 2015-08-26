f0800aa1ca51a705c90cde9cd98141917ac85f8f
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-08-25 22:40:11 -0400

    install all apps now only installs apps that are being followed

--- app/loggedIn/applications/applicationManager/applicationManager.ctrl.js
+++ app/loggedIn/applications/applicationManager/applicationManager.ctrl.js
@@ -10,6 +10,13 @@ window.thisApp.controller('ApplicationCtrl', function ($scope, $state, $rootScop
 		return 	ApplicationFactory.retrieveCurrentApps()
 		.then(function(apps){
 			$scope.currentCask = apps;
+			// // return array with just the names of the current apps
+			// $scope.currentUploadedNames = []
+			// $scope.currentCask.forEach(function(appObj){
+			// 	console.log("appObj.name: ", appObj.name)
+			// 	$scope.currentUploadedNames.push(appObj.name)
+			// })
+			// console.log("$scope.currentUploadedNames: ", $scope.currentUploadedNames)
 			$scope.$digest();
 		})
 	}
@@ -19,11 +26,37 @@ window.thisApp.controller('ApplicationCtrl', function ($scope, $state, $rootScop
 	$scope.syncAppsFromComputer = function(){
 		ApplicationFactory.uploadFinderInstalled()
 		.then(function(apps){
+			// define apps listed in user's finder and brew cask, then a list of both without repeats
 			var finderApps = apps
 			var caskApps = ApplicationFactory.uploadCaskInstalled()
 			var chosenApps = _.uniq(finderApps.concat(caskApps))
-			ApplicationFactory.addAppsToUser(chosenApps)
+
+			console.log("$scope.updateCurrent: ", $scope.currentCask)
+
+			// return array with just the names of the current apps
+			// $scope.currentUploadedNames = []
+			// $scope.currentCask.forEach(function(appObj){
+			// 	console.log("appObj.name: ", appObj.name)
+			// 	$scope.currentUploadedNames.push(appObj.name)
+			// })
+			// $scope.$digest()
+			// console.log("$scope.currentUploadedNames: ", $scope.currentUploadedNames)
+			
+			var appsToAdd = chosenApps
+			// console.log("$scope.currentUploadedNames: ", $scope.currentUploadedNames)
+
+			// var appsToAdd = _.difference(chosenApps, $scope.currentUploadedNames)
+			console.log("appsToAdd: ", appsToAdd)
+
+			
+			// var appsToAdd = []
+			// var nonDuplicateApps = chosenApps.forEach(function(app){
+			// 	var indexOfMatch = $scope.currentUploadedNames.indexOf(app)
+			// 	if (indexOfMatch != -1) appsToAdd.push($scope.currentUploadedNames[indexOfMatch]);
+			// })
+			ApplicationFactory.addAppsToUser(appsToAdd)
 			.then(function(apps){
+				console.log("current apps: ", apps)
 				$scope.updateCurrentApps();
 				$scope.$digest();
 			});
@@ -53,7 +86,7 @@ window.thisApp.controller('ApplicationCtrl', function ($scope, $state, $rootScop
 	}
 
 	$scope.installApp = function(app){
-		ApplicationFactory.installApp(app.name);
+		ApplicationFactory.installApp(app)
 	}
 
 	$scope.installAllApps = function(){
